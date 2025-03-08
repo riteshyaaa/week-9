39d761341f8bc70fcbf425d1c2621d217640287c
riuteshyaaa <riteshyaaa@gmail.com>
2025-03-08 22:59:33 +0530

    initialize express application in index.js

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div> 
+        <h1>Counter App</h1>
+        <h2> Counter {count}</h2>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+       
+    
+  )
+}
+
+export default App
+
