ddda635c71e44404c2bcc013c4b7b972d309dcc0
riuteshyaaa <riteshyaaa@gmail.com>
2025-03-22 19:24:05 +0530

    linkdin tabBar

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,7 +1,5 @@
-import { useEffect, useState, } from 'react'
-import './App.css'
-
-
+import { useEffect, useState } from "react";
+import "./App.css";
 
 // export default function App () {
 //   const [ count, setCount ] =  useState(0)
@@ -23,34 +21,31 @@ import './App.css'
 //   )
 // }
 
-
 // const App =  () => {
 // const [CounterVisible, setCounterVisible ] =  useState(true);
 
 // useEffect(() => {
 //   setInterval ( () => {
-//     setCounterVisible(CounterVisible => !CounterVisible) 
+//     setCounterVisible(CounterVisible => !CounterVisible)
 //   },6000 )
-    
-//   },[]) 
+
+//   },[])
 
 //   return(
 //     <div>
 //         { CounterVisible && <Counter /> }
 //         <h2>  Counter will invisible after   sec (Conditional rendering) </h2>
-         
-     
+
 //     </div>
 //   )
 // }
-
 
 // const  Counter =  () => {
 
 //   const [count, SetCount] =  useState(0)
 
 //   useEffect( () => {
-    
+
 //   setInterval( () => {
 //     SetCount( count => count +1)
 //   },1000)
@@ -60,53 +55,106 @@ import './App.css'
 //     <div>
 //       <h1> Counter {count}</h1>
 //       <button> Counter button is not working at this point of time </button>
-      
+
 //     </div>
 //   )
-
 
 // }
 // export default App
 
+// const App = () => {
+//   const [counterVisible, setCounterVisible] = useState(true);
+//   const [count, setCount] = useState(0); // Lifted state to App
+
+//   useEffect(() => {
+//     // Toggle visibility every 6 seconds
+//     const interval = setInterval(() => {
+//       setCounterVisible((counterVisible) => !counterVisible);
+//     }, 6000);
+
+//     return () => clearInterval(interval); // Cleanup interval
+//   }, []);
+
+//   useEffect(() => {
+//     // Update count every second
+//     const countInterval = setInterval(() => {
+//       setCount((prev) => prev + 1);
+//     }, 1000);
+
+//     return () => clearInterval(countInterval); // Cleanup interval
+//   }, []);
+
+//   useEffect(() => {
+//     console.log("It'll be run when ever your count variable changes "+ count)
+//   })
+//   return (
+//     <div>
+//       {counterVisible && <Counter count={count} />}
+//       <h2>Counter will be invisible after a few seconds (Conditional Rendering)</h2>
+//     </div>
+//   );
+// };
+
+// const Counter = ({ count }) => {
+//   return (
+//     <div>
+
+//       <h1>Counter: {count}</h1>
+//       <button>Counter button is not {count}working at this point in time</button>
+//     </div>
+//   );
+// };
+
+// export default App;
 
 const App = () => {
-  const [counterVisible, setCounterVisible] = useState(true);
-  const [count, setCount] = useState(0); // Lifted state to App
-
+  const [currentTab, setCurrentTab] = useState(1);
+  const [tabData, setTabData] = useState(1);
   useEffect(() => {
-    // Toggle visibility every 6 seconds
-    const interval = setInterval(() => {
-      setCounterVisible((counterVisible) => !counterVisible);
-    }, 6000);
-
-    return () => clearInterval(interval); // Cleanup interval
-  }, []);
-
-  useEffect(() => {
-    // Update count every second
-    const countInterval = setInterval(() => {
-      setCount((prev) => prev + 1);
-    }, 1000);
-
-    return () => clearInterval(countInterval); // Cleanup interval
-  }, []);
-
+    //send a request to the backend server
+    fetch("https://jsonplaceholder.typicode.com/todos/" + currentTab).then(
+      async (response) => {
+        const json = await response.json();
+        setTabData(json);
+      }
+    );
+  }, [currentTab]);
   return (
     <div>
-      {counterVisible && <Counter count={count} />}
-      <h2>Counter will be invisible after a few seconds (Conditional Rendering)</h2>
+      <button
+        onClick={() => {
+          setCurrentTab(1);
+        }}
+        style={{ backgroundColor: currentTab === 1 ? "blue" : "white" }}
+      >
+        Todo #1{" "}
+      </button>
+      <button
+        onClick={() => {
+          setCurrentTab(2);
+        }}
+        style={{ backgroundColor: currentTab === 2 ? "blue" : "white" }}
+      >
+        Todo #2
+      </button>
+      <button
+        onClick={() => {
+          setCurrentTab(3);
+        }}
+        style={{ backgroundColor: currentTab === 3 ? "blue" : "white" }}
+      >
+        Todo #3
+      </button>
+      <button
+        onClick={() => {
+          setCurrentTab(4);
+        }}
+        style={{ backgroundColor: currentTab === 4 ? "blue" : "white" }}
+      >
+        Todo #4
+      </button>
+      {tabData && <div> {tabData.title} </div>}
     </div>
   );
 };
-
-const Counter = ({ count }) => {
-  return (
-    <div>
-      
-      <h1>Counter: {count}</h1>
-      <button>Counter button is not {count}working at this point in time</button>
-    </div>
-  );
-};
-
 export default App;
